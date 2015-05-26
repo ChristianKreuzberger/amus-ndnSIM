d225e0e437972d424ccca0013afb7babc562669e
ChristianKreuzberger <christian.kreuzberger@itec.aau.at>
2015-05-26 09:07:48 +0200

    Updated Licence etc... in all files

--- utils/multimedia/adaptation-logic-factory.hpp
+++ utils/multimedia/adaptation-logic-factory.hpp
@@ -1,19 +1,21 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /**
- * Copyright (c) 2015 - Christian Kreuzberger - based on ndnSIM
+ * Copyright (c) 2015 Christian Kreuzberger and Daniel Posch, Alpen-Adria-University 
+ * Klagenfurt
  *
- * This file is part of amus-ndnSIM. See AUTHORS for complete list of ndnSIM authors and
- * contributors.
+ * This file is part of amus-ndnSIM, based on ndnSIM. See AUTHORS for complete list of 
+ * authors and contributors.
  *
- * ndnSIM is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ * amus-ndnSIM and ndnSIM are free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * amus-ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE.  See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ * amus-ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
 #ifndef DASH_ADAPTATION_LOGIC_FACTORY
